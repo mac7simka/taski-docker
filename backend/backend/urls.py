296537953d722e39043f896f211bb6de051282b2
Maksim Karaskin <mac7sim@gmail.com>
2024-08-08 11:23:09 +0300

    isort

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,7 @@
-from api import views
 from django.contrib import admin
 from django.urls import include, path
+
+from api import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
